93ca7c3d0182d98948f5d44a970ed299baf259bb
hinacay <hinacay@fullscale.io>
2023-03-07 21:25:07 +0800

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,4 +29,6 @@ namespace BankTransactions.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    //sample
 }
